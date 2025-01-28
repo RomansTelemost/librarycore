945a6f5cd368d7cd817234e8d7181a58aa20c0e2
Roman <leo_dialog@mail.ru>
2025-01-28 22:16:02 +0300

    Optimize sql query while filling books in author

--- src/main/java/org/romanco/library/librarycore/entity/Author.java
+++ src/main/java/org/romanco/library/librarycore/entity/Author.java
@@ -1,9 +1,6 @@
 package org.romanco.library.librarycore.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,90 +9,13 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 public class Author {
-
     private Long id;
     private String firstName;
     private String lastName;
     private String middleName;
     private LocalDate birthday;
     private LocalDate deathDate;
-
     private List<Book> books;
-
-    public Author(AuthorBuilder authorBuilder) {
-        if (authorBuilder == null) {
-            throw new IllegalArgumentException("AuthorBuilder has not found!");
-        }
-        this.id = authorBuilder.id;
-        this.firstName = authorBuilder.firstName;
-        this.lastName = authorBuilder.lastName;
-        this.middleName = authorBuilder.middleName;
-        this.birthday = authorBuilder.birthday;
-        this.deathDate = authorBuilder.deathDate;
-        this.books = authorBuilder.books;
-    }
-
-    public static class AuthorBuilder {
-        private Long id;
-        private String firstName;
-        private String lastName;
-        private String middleName;
-        private LocalDate birthday;
-        private LocalDate deathDate;
-
-        private List<Book> books;
-
-        public AuthorBuilder() {
-            super();
-        }
-
-        public AuthorBuilder authorId(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public AuthorBuilder authorFirstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-
-        public AuthorBuilder authorLastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-
-        public AuthorBuilder authorMiddleName(String middleName) {
-            this.middleName = middleName;
-            return this;
-        }
-
-        public AuthorBuilder authorBirthday(LocalDate birthday) {
-            this.birthday = birthday;
-            return this;
-        }
-
-        public AuthorBuilder authorDeathDate(LocalDate deathDate) {
-            this.deathDate = deathDate;
-            return this;
-        }
-
-        public AuthorBuilder authorBooks(List<Book> books) {
-            this.books = books;
-            return this;
-        }
-
-        public Author build() {
-            Author author;
-            if (validateAuthor()) {
-                author = new Author(this);
-            } else {
-                throw new IllegalArgumentException("Не указаны необходимые параметры");
-            }
-            return author;
-        }
-        private boolean validateAuthor() {
-            return (id != 0);
-        }
-    }
 }
--- src/main/java/org/romanco/library/librarycore/repository/AuthorRepositoryImpl.java
+++ src/main/java/org/romanco/library/librarycore/repository/AuthorRepositoryImpl.java
@@ -2,28 +2,45 @@ package org.romanco.library.librarycore.repository;
 
 import lombok.RequiredArgsConstructor;
 import org.romanco.library.librarycore.entity.Author;
+import org.romanco.library.librarycore.entity.Book;
 import org.romanco.library.librarycore.exception.SqlProcessingException;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.Stack;
+import java.util.*;
 
 @Repository
 @RequiredArgsConstructor
 public class AuthorRepositoryImpl implements AuthorRepository {
 
     private final static String INSERT_AUTHOR = "INSERT INTO author(id, firstName, lastName, middleName, birthday, deathDate) VALUES (?, ?, ?, ?, ?, ?)";
-    private static final String SELECT_AUTHOR_BY_ID = "SELECT * FROM author WHERE id = ?";
-    private static final String SELECT_ALL_AUTHOR = "SELECT * FROM author";
+    private static final String SELECT_AUTHOR_BY_ID = "SELECT " +
+            "author.id AS author_id, " +
+            "author.first_name, " +
+            "author.last_name, " +
+            "author.middle_name, " +
+            "author.birthday, " +
+            "author.death_date, " +
+            "book.id AS book_id, " +
+            "book.title " +
+            "FROM author " +
+            "LEFT JOIN book ON author.id = book.author_id " +
+            "WHERE author_id = ?";
+    private static final String SELECT_ALL_AUTHOR = "SELECT " +
+            "author.id AS author_id, " +
+            "author.first_name, " +
+            "author.last_name, " +
+            "author.middle_name, " +
+            "author.birthday, " +
+            "author.death_date, " +
+            "book.id AS book_id, " +
+            "book.title " +
+            "FROM author " +
+            "LEFT JOIN book ON author.id = book.author_id";
     private final static String DELETE_BY_ID = "DELETE FROM author where id = ?";
 
     private final DataSource dataSource;
-
-    private final BookRepository bookRepository;
 
     @Override
     public void save(Author author) {
@@ -45,15 +62,25 @@ public class AuthorRepositoryImpl implements AuthorRepository {
 
     @Override
     public List<Author> findAll() {
-        List<Author> result = new ArrayList<>();
+        Map<Long, Author> authorResult = new HashMap<>();
+
         try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_AUTHOR)) {
 
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
-                result.add(fillAuthor(rs));
+                Book book = fillBook(rs);
+                Long authorId = rs.getLong("author_id");
+                Author author;
+                if (authorResult.containsKey(authorId)) {
+                    author = authorResult.get(authorId);
+                } else {
+                    author = fillAuthor(rs);
+                    authorResult.put(authorId, author);
+                }
+                author.getBooks().add(book);
             }
-            return result;
+            return new ArrayList<>(authorResult.values());
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -63,12 +90,16 @@ public class AuthorRepositoryImpl implements AuthorRepository {
     public Optional<Author> findById(Long id) {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(SELECT_AUTHOR_BY_ID)) {
-
+            preparedStatement.setLong(1, id);
             ResultSet rs = preparedStatement.executeQuery();
-            if (rs.next()) {
-                return Optional.of(fillAuthor(rs));
+            Author author = null;
+            while (rs.next()) {
+                if (author == null) {
+                    author = fillAuthor(rs);
+                }
+                author.getBooks().add(fillBook(rs));
             }
-            return Optional.empty();
+            return Optional.ofNullable(author);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -85,15 +116,22 @@ public class AuthorRepositoryImpl implements AuthorRepository {
         }
     }
 
-    private Author fillAuthor(ResultSet rs) throws SQLException{
+    private Author fillAuthor(ResultSet rs) throws SQLException {
         Author author = new Author();
-        author.setId(rs.getLong("id"));
+        author.setId(rs.getLong("author_id"));
         author.setFirstName(rs.getString("first_name"));
         author.setLastName(rs.getString("last_name"));
         author.setMiddleName(rs.getString("middle_name"));
 //                author.setBirthday(rs.getDate("id"));
 //                author.setDeathDate(rs.getDate("id"));
-        author.setBooks(bookRepository.findByAuthorId(author.getId()));
+        author.setBooks(new ArrayList<>());
         return author;
+    }
+
+    private Book fillBook(ResultSet rs) throws SQLException {
+        Book book = new Book();
+        book.setId(rs.getLong("book_id"));
+        book.setTitle(rs.getString("title"));
+        return book;
     }
 }
