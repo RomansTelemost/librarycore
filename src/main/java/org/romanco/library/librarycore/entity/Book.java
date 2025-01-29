2fda40d38921dc70625f56070ae6f255c7f8bf43
Roman <leo_dialog@mail.ru>
2025-01-30 00:33:29 +0300

    Try @Query annotation for fetch books instead @EntityGraph.

--- src/main/java/org/romanco/library/librarycore/entity/Book.java
+++ src/main/java/org/romanco/library/librarycore/entity/Book.java
@@ -3,13 +3,7 @@ package org.romanco.library.librarycore.entity;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.*;
 
 @NoArgsConstructor
--- src/main/java/org/romanco/library/librarycore/repository/AuthorRepository.java
+++ src/main/java/org/romanco/library/librarycore/repository/AuthorRepository.java
@@ -3,6 +3,7 @@ package org.romanco.library.librarycore.repository;
 import org.romanco.library.librarycore.entity.Author;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,6 +15,6 @@ public interface AuthorRepository extends JpaRepository<Author, Long> {
     List<Author> findAll();
 
     @Override
-    @EntityGraph(type = EntityGraph.EntityGraphType.FETCH, attributePaths = "books")
+    @Query(value = "SELECT a FROM Author a join fetch a.books b WHERE a.id = :aLong")
     Optional<Author> findById(Long aLong);
 }
