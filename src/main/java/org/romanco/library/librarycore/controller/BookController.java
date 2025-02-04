ac20f5486ad2bc9aacff881c36503c5ca4e055f4
Roman <leo_dialog@mail.ru>
2025-02-04 22:15:23 +0300

    Fix createBook endpoint in BookController.

--- src/main/java/org/romanco/library/librarycore/controller/BookController.java
+++ src/main/java/org/romanco/library/librarycore/controller/BookController.java
@@ -6,14 +6,7 @@ import org.romanco.library.librarycore.mapper.BookMapper;
 import org.romanco.library.librarycore.service.BookService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1")
@@ -23,8 +16,8 @@ public class BookController {
     private final BookService bookService;
     private final BookMapper bookMapper;
 
-    @PostMapping("/book")
-    public ResponseEntity<Void> createBook(@RequestParam(name = "authorId") Long authorId, @RequestBody BookDto bookDto) {
+    @PostMapping("author/{authorId}/book")
+    public ResponseEntity<Void> createBook(@PathVariable (name = "authorId") Long authorId, @RequestBody BookDto bookDto) {
         bookService.createBook(authorId, bookMapper.toEntity(bookDto));
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
