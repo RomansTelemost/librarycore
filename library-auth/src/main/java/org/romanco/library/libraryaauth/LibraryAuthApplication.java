43062ff7948b4c2ea2df4f94f000d0856fd8f623
Roman <leo_dialog@mail.ru>
2025-06-25 15:55:59 +0300

    create new package for auth

---
+++ library-auth/src/main/java/org/romanco/library/libraryaauth/LibraryAuthApplication.java
@@ -0,0 +1,13 @@
+package org.romanco.library.libraryaauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAuthApplication.class, args);
+	}
+
+}
--- library-auth/src/main/java/org/romanco/library/libraryaauth/config/SecurityConfig.java
+++ library-auth/src/main/java/org/romanco/library/libraryaauth/config/SecurityConfig.java
@@ -1,15 +1,17 @@
-package org.romanco.library.librarycore.config;
+package org.romanco.library.libraryaauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
