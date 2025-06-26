f76b3afb5e077d96ebb296877554db80c306761a
Roman <leo_dialog@mail.ru>
2025-06-26 20:50:03 +0300

    temp application_user_account

--- library-auth/src/main/java/org/romanco/library/libraryaauth/service/RegistrationServiceImpl.java
+++ library-auth/src/main/java/org/romanco/library/libraryaauth/service/RegistrationServiceImpl.java
@@ -6,6 +6,7 @@ import org.romanco.library.libraryaauth.dto.ApplicationUserDto;
 import org.romanco.library.libraryaauth.mapper.UserMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,8 +19,10 @@ public class RegistrationServiceImpl implements RegistrationService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public void registerUser(ApplicationUserDto applicationUserDto) {
         applicationUserDto.setPassword(bCryptPasswordEncoder.encode(applicationUserDto.getPassword()));
         userRepository.save(userMapper.toUser(applicationUserDto));
+        userRepository.save();
     }
 }
