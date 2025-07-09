47a7182f3e7bd046bfe72eab2885e50b7e2c236b
Roman <leo_dialog@mail.ru>
2025-07-09 20:13:01 +0300

    add jwtAuth core service done

--- library-core/src/main/java/org/romanco/library/librarycore/config/SecurityConfig.java
+++ library-core/src/main/java/org/romanco/library/librarycore/config/SecurityConfig.java
@@ -1,18 +1,23 @@
 package org.romanco.library.librarycore.config;
 
+import org.romanco.library.librarycore.filter.JwtFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.secret}")
+    private String secret;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -20,8 +25,10 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((requests) -> requests
-                        .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+                        .anyRequest()
+                        .authenticated())
+                .addFilterBefore(new JwtFilter(secret), BasicAuthenticationFilter.class);
+
         return http.build();
     }
 
---
+++ library-core/src/main/java/org/romanco/library/librarycore/filter/JwtFilter.java
@@ -0,0 +1,49 @@
+package org.romanco.library.librarycore.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private static final String TOKEN_PREFIX = "Bearer ";
+    private final String secret;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String headerValue = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (headerValue == null || !headerValue.startsWith(TOKEN_PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        String jwtToken = headerValue.substring(TOKEN_PREFIX.length());
+        try {
+
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secret).parseClaimsJws(jwtToken);
+            Claims body = claimsJws.getBody();
+            String subject = body.getSubject();
+            UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                    new UsernamePasswordAuthenticationToken(subject, null, Collections.emptySet());
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            filterChain.doFilter(request, response);
+
+        } catch (JwtException e) {
+            throw new IllegalArgumentException("Token is invalid %s".formatted(jwtToken));
+        }
+    }
+}
