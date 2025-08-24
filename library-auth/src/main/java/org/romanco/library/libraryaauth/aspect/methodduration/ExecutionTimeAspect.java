5632436971a290fc3ec0fe81a744a19b1e53012b
Roman <leo_dialog@mail.ru>
2025-08-24 20:31:12 +0300

    assume execution time aop

---
+++ library-auth/src/main/java/org/romanco/library/libraryaauth/aspect/methodduration/ExecutionTimeAspect.java
@@ -0,0 +1,30 @@
+package org.romanco.library.libraryaauth.aspect.methodduration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class ExecutionTimeAspect {
+
+    @Around("@annotation(LogExecutionTime)")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        log.info("Method {} executed in {} ms",
+                joinPoint.getSignature(),
+                executionTime);
+
+        return result;
+    }
+
+}
